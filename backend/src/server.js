97d582d76312107f30609f08846c870289c9cce4
Kristiano <kristiano.melo@tacom.com.br>
2019-10-01 11:24:30 -0300

    Primeiro Commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-8bznx.mongodb.net:27017,omnistack-shard-00-01-8bznx.mongodb.net:27017,omnistack-shard-00-02-8bznx.mongodb.net:27017/semana09?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route params (para edição e delete)
+//req.body = Acessar corpo da requisição (para criação de registros)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
