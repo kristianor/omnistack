e38afd5ee1ca5c31c77899c56255e7b8961d9304
kristianor <kristianor@hotmail.com>
2019-10-03 18:07:58 -0300

    frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-8bznx.mong
 //req.params = Acessar route params (para edição e delete)
 //req.body = Acessar corpo da requisição (para criação de registros)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
