2edf4739a454eca8670131033f58a4eeed9c6a9e
Kristiano <kristiano.melo@tacom.com.br>
2019-10-07 18:29:07 -0300

    Ajuste no server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-8bznx.mongodb.net:27017,omnistack-shard-00-01-8bznx.mongodb.net:27017,omnistack-shard-00-02-8bznx.mongodb.net:27017/semana09?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+
 //req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição e delete)
 //req.body = Acessar corpo da requisição (para criação de registros)
 
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+});
+
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
